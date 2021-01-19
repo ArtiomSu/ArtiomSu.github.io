774c3a5aa09f6aa972fc5a737a22e8aed8e7018e
ArtiomSu <artiom.sumigora@mycit.ie>
2021-01-19 02:20:43 +0000

    created single page app functionality, made theme picker nicer

--- javascripts/index.js
+++ javascripts/index.js
@@ -14,6 +14,8 @@ $(document).ready(function(){
     var matrix_text_colour = '#ef741b';
     var rainbow_matrix = false;
 
+
+
     var drawMatrix = function () {
         matrix_canvas.getContext('2d').fillStyle='rgba(0,0,0,.1)';
         matrix_canvas.getContext('2d').fillRect(0,0,width,height);
@@ -49,7 +51,7 @@ $(document).ready(function(){
                 headerProgressBarInner.width(headerProgressBarInner_width+'%');
                 headerProgressBarInner_width++;
             }else {
-                headerProgressBarInner.animate({"opacity": '0%'});
+                headerProgressBarInner.animate({"opacity": '0%',width: '0%'});
             }
         }
 
@@ -58,27 +60,117 @@ $(document).ready(function(){
     };
     setInterval(drawMatrix, 60);
 
-    $("#matrix_canvas_orange").click(function(){
+    $("#matrix_canvas_orange").hover(function(){
         matrix_text_colour = '#ef741b';
         rainbow_matrix = false;
+    }, function (){
     });
-
-    $("#matrix_canvas_red").click(function(){
+    $("#matrix_canvas_red").hover(function(){
         matrix_text_colour = '#78080b';
         rainbow_matrix = false;
+    }, function (){
     });
-    $("#matrix_canvas_blue").click(function(){
+    $("#matrix_canvas_blue").hover(function(){
         matrix_text_colour = '#102c8e';
         rainbow_matrix = false;
+    }, function (){
     });
-    $("#matrix_canvas_green").click(function(){
+    $("#matrix_canvas_green").hover(function(){
         matrix_text_colour = '#379a0b';
         rainbow_matrix = false;
+    }, function (){
     });
-    $("#matrix_canvas_rainbow").click(function(){
+    $("#matrix_canvas_rainbow").hover(function(){
         matrix_text_colour = '#ef741b';
         rainbow_matrix = true;
+    }, function (){
     });
+
+    $("#matrix_colour_btns_div_container").hover(function (){
+        var animation_transition = 500;
+        //$("#matrix_colour_btns_div").animate({height:"30px"},animation_transition);
+
+        $("#matrix_canvas_theme").animate({"background-position-y":"125%"},animation_transition);
+
+        $("#matrix_canvas_orange").animate({"fontSize": "15px",height: "30px"},animation_transition);
+        $("#matrix_canvas_red").animate({"fontSize": "15px",height: "30px"},animation_transition+100);
+        $("#matrix_canvas_blue").animate({"fontSize": "15px",height: "30px"},animation_transition+200);
+        $("#matrix_canvas_green").animate({"fontSize": "15px",height: "30px"},animation_transition+300);
+        $("#matrix_canvas_rainbow").animate({"fontSize": "15px",height: "30px"},animation_transition+400);
+
+    }, function (){
+        var animation_transition = 500;
+        //$("#matrix_colour_btns_div").animate({height:"2px"},animation_transition);
+        $("#matrix_canvas_theme").animate({"background-position-y":"100%"},animation_transition+400);
+        $("#matrix_canvas_orange").animate({"fontSize": "0px",height: "2px"},animation_transition+400);
+        $("#matrix_canvas_red").animate({"fontSize": "0px",height: "2px"},animation_transition+300);
+        $("#matrix_canvas_blue").animate({"fontSize": "0px",height: "2px"},animation_transition+200);
+        $("#matrix_canvas_green").animate({"fontSize": "0px",height: "2px"},animation_transition+100);
+        $("#matrix_canvas_rainbow").animate({"fontSize": "0px",height: "2px"},animation_transition);
+        //$(this).animate({height:"5px"},1000);
+    });
+
+
+
+    get_container("AsusRogPhoneRGB.html","containers");
+
+    //html containers
+    $("#container_asusrogphonergb").click(function (){
+        animate_get_container("AsusRogPhoneRGB.html","containers");
+    });
+    $("#container_dynamicrypt").click(function (){
+        animate_get_container("DynamiCrypt.html","containers");
+    });
+    $("#container_qmk").click(function (){
+        animate_get_container("QMK.html","containers");
+    });
+
+
+
+
+    $(window).resize(function() {
+        $("#containers").css("height",$(window).height()-320);
+    });
+
 
 
 });
+
+function animate_get_container(container_name,div_id){
+    httpGetAsync("/containers/"+container_name,animate_get_container_callback,div_id);
+}
+
+function animate_get_container_callback(html,div_id){
+    $("#"+div_id).animate({opacity:"0%",marginLeft:"150px"},600,function (){
+        var window_before = $(window).height();
+        console.log("window height before:",window_before);
+        $("#"+div_id).html(html);
+        console.log("window height after:",$(window).height());
+        $("#"+div_id).css("height",$(window).height()-320);
+        //document.getElementById(div_id).innerHTML = html;
+        $("#"+div_id).animate({opacity:"100%",marginLeft:"0px"},600,function (){
+            //$('#'+div_id).attr('overflowy','scroll');
+        });
+    });
+}
+
+
+function get_container(container_name,div_id){
+    httpGetAsync("/containers/"+container_name,get_container_callback,div_id);
+}
+
+function get_container_callback(html,div_id){
+    $("#"+div_id).html(html);
+    //document.getElementById(div_id).innerHTML = html;
+}
+
+function httpGetAsync(theUrl, callback,div_id)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText,div_id);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
+}
