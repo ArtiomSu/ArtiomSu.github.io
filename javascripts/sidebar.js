ba7b1b58a8919ec1b09bac64adfc0ab2f5af5b7c
ArtiomSu <artiom.sumigora@mycit.ie>
2021-01-27 00:53:19 +0000

    added iteminner support. Added entry for arduino Custom mouse

--- javascripts/sidebar.js
+++ javascripts/sidebar.js
@@ -2,10 +2,11 @@ var is_categorie_1_expanded = false;
 var current_expanded_categorie = null;
 var current_expanded_categorie_list = null;
 var current_sub_categorie_clicked = null;
+var current_inner_sub_categorie_clicked = null;
 
 
 $(document).ready(function(){
-    var projects_div_names = ["container_asusrogphonergb","container_dynamicrypt","container_qmk"];
+    var projects_div_names = ["container_asusrogphonergb","container_dynamicrypt","container_qmk","container_arduinocustommouse"];
     var youtube_div_names = ["container_Youtube1", "container_Youtube2", "container_Youtube3", "container_Youtube4", "container_Youtube5", "container_Youtube6", "container_Youtube7", "container_Youtube8", "container_Youtube9", "container_Youtube10", "container_Youtube11", "container_Youtube12", "container_Youtube13", "container_Youtube14"];
 
 
@@ -13,6 +14,7 @@ $(document).ready(function(){
         expand_collapse_categories("#container_home",null);
         animate_get_container("Home.html","containers");
         urlInsertParam("cat","container_home");
+        urlInsertParam("iteminner",null);
         urlInsertParam("catonly",1);
     });
 
@@ -20,22 +22,32 @@ $(document).ready(function(){
     $("#container_Projects").click(function (){
         expand_collapse_categories("#container_Projects",projects_div_names);
         urlInsertParam("cat","container_Projects");
+        urlInsertParam("iteminner",null);
         urlInsertParam("catonly",1);
     });
     $("#container_asusrogphonergb").click(function (){
         sub_categorie_click("AsusRogPhoneRGB.html","containers", "#container_asusrogphonergb");
         urlInsertParam("item","container_asusrogphonergb");
+        urlInsertParam("iteminner",null);
         urlInsertParam("catonly",0);
     });
     $("#container_dynamicrypt").click(function (){
         sub_categorie_click("DynamiCrypt.html","containers", "#container_dynamicrypt");
         urlInsertParam("item","container_dynamicrypt");
+        urlInsertParam("iteminner",null);
         urlInsertParam("catonly",0);
     });
     $("#container_qmk").click(function (){
         sub_categorie_click("QMK.html","containers", "#container_qmk");
         urlInsertParam("item","container_qmk");
+        urlInsertParam("iteminner",null);
         urlInsertParam("catonly",0);
+    });
+    $("#container_arduinocustommouse").click(function (){
+        click_container_arduinocustommouse();
+    });
+    $("#container_arduinocustommousecode").click(function (){
+        click_container_arduinocustommousecode();
     });
 
 
@@ -44,28 +56,72 @@ $(document).ready(function(){
         expand_collapse_categories("#container_Youtube",youtube_div_names);
         urlInsertParam("cat","container_Youtube");
         urlInsertParam("catonly",1);
+        urlInsertParam("iteminner",null);
     });
 
-    //deal with cookies
-    var last_visited_href = getCookie("last_visited");
-    if ( last_visited_href !== ""){
-        window.history.pushState({path:last_visited_href},'',last_visited_href);
-    }
-
-
     //deal with url params
+    var use_cookie = true;
     var urlParams = new URLSearchParams(window.location.search);
     if(urlParams.has("cat")){
-        console.log(urlParams.get("cat"));
+        use_cookie = false;
         $("#"+urlParams.get("cat")).click();
     }
     if(urlParams.has("item") && urlParams.has("catonly")){
         if(urlParams.get("catonly") === "0"){
+            use_cookie = false;
             $("#"+urlParams.get("item")).click();
+        }
+    }
+    if(urlParams.has("item") && urlParams.has("iteminner")){
+        setTimeout(function() {
+            switch (urlParams.get("iteminner")){
+                case "container_arduinocustommousecode":
+                    click_container_arduinocustommousecode();
+                    break;
+                default:
+
+            }
+        }, 100);
+    }
+
+    if(use_cookie){
+        //deal with cookies
+        var last_visited_href = getCookie("last_visited");
+        if ( last_visited_href !== ""){
+            window.history.pushState({path:last_visited_href},'',last_visited_href);
         }
     }
 
 });
+
+function click_container_arduinocustommousecode(){
+    sub_inner_categorie_click("ArduinoCustomMouseCode.html","containers", "#container_arduinocustommousecode");
+    urlInsertParam("iteminner","container_arduinocustommousecode");
+    urlInsertParam("catonly",0);
+}
+
+function click_container_arduinocustommouse(){
+    sub_categorie_click("ArduinoCustomMouse.html","containers", "#container_arduinocustommouse");
+    urlInsertParam("item","container_arduinocustommouse");
+    urlInsertParam("iteminner",null);
+    urlInsertParam("catonly",0);
+}
+
+function sub_inner_categorie_click(html_page, container_div, name){
+    var send_request = true;
+    if(current_inner_sub_categorie_clicked === null){
+        current_inner_sub_categorie_clicked = name;
+    }else if(current_inner_sub_categorie_clicked !== name){
+
+        current_inner_sub_categorie_clicked = name;
+    }else if(current_inner_sub_categorie_clicked === name){
+        send_request = false;
+    }
+    if(send_request){
+        animate_get_container(html_page,container_div);
+    }
+}
+
 
 function sub_categorie_click(html_page, container_div, name){
     var send_request = true;
@@ -78,6 +134,10 @@ function sub_categorie_click(html_page, container_div, name){
         current_sub_categorie_clicked = name;
     }else if(current_sub_categorie_clicked === name){
         send_request = false;
+    }
+    if(current_inner_sub_categorie_clicked !== null){
+        current_inner_sub_categorie_clicked = null;
+        send_request = true;
     }
     if(send_request){
         animate_get_container(html_page,container_div);
