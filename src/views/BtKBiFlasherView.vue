66e7c73b36e9ad1d15e93d014114c13e73a7d55b
ArtiomSu <doasartiom@gmail.com>
2026-04-01 15:58:18 +0100

    try using proxy for downloading manifests for the flasher

--- src/views/BtKBiFlasherView.vue
+++ src/views/BtKBiFlasherView.vue
@@ -53,7 +53,7 @@
 <script setup>
 import { ref } from "vue";
 
-const baseUrl = "https://github.com/ArtiomSu/bluetooth-to-keyboard-input-esp32-s3-zero/releases/latest/download";
+const baseUrl = "https://corsproxy.io/?url=https://github.com/ArtiomSu/bluetooth-to-keyboard-input-esp32-s3-zero/releases/latest/download";
 
 const normalInstaller = ref(null);
 const eraseInstaller = ref(null);
